0ecd16f17fa5ebfaf049c310283136562db02e3b
AmanPr01 <me.aman.pr@gmail.com>
2024-08-15 19:23:20 +0530

    Service Discovery

---
+++ src/main/java/dev/aman/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,21 @@
+package dev.aman.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+
+}
+
+
+/*
+https://spring.io/projects/spring-cloud-netflix#overview
+ */
+// For context go to the above link.
